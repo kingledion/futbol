231bdab079f35230b18ac48faccbbd06127b6769
Daniel Hartig <dhartig@Daniels-MacBook-Pro.local>
2020-01-13 09:01:58 -0600

    Added Bundesliga and Bundesliga 2 back to 2014. Next project is having new added teams conserve score; i.e. start with a lower 'expected' rating.

--- python/parser_fbref.py
+++ python/parser_fbref.py
@@ -16,34 +16,33 @@ def parseSeason(teamlist: TeamList, filename, leaguename, leagueyear):
         game_rdr = csv.reader(games_in)
         row = next(game_rdr)
 
-        PREPEND_COL = False
-        HAS_XG = False
+        table_formats = set([])
         if row == ['Wk', 'Day', 'Date', 'Time', 'Home', 'Score', 'Away', 'Attendance', 'Venue', 'Referee', 'Match Report', 'Notes']:
             pass
         elif row == ['Wk', 'Day', 'Date', 'Time', 'Home', 'xG', 'Score', 'xG', 'Away', 'Attendance', 'Venue', 'Referee', 'Match Report', 'Notes']:
-            HAS_XG = True
+            table_formats = {"XG"}
         elif row == ['Round', 'Wk', 'Day', 'Date', 'Time', 'Home', 'Score', 'Away', 'Attendance', 'Venue', 'Referee', 'Match Report', 'Notes']:
-            PREPEND_COL = True
+            table_formats = {"Round"}
+        elif row == ['Round', 'Wk', 'Day', 'Date', 'Time', 'Home', 'xG', 'Score', 'xG', 'Away', 'Attendance', 'Venue', 'Referee', 'Match Report', 'Notes']:
+            table_formats = {"XG", "Round"}
         else:
             raise RuntimeError("Unable to parse table with header row: {}".format(row))
 
         for row in game_rdr:
 
-            HAS_POSTSEASON = False
-
-            if HAS_XG:
-                try:
+            if "XG" in table_formats:
+                if "Round" in table_formats:
+                    rg_season, gamedate, homename, homexg, score, awayxg, awayname = row[0], row[3], row[5], row[6], row[7], row[8], row[9]
+                else:
                     gamedate, homename, homexg, score, awayxg, awayname = row[2], row[4], row[5], row[6], row[7], row[8]
-                except KeyError as e:
-                    print(row)
-                    raise e
-            elif PREPEND_COL:
-                HAS_POSTSEASON = True
-                rg_season, gamedate, homename, score, awayname = row[0], row[3], row[5], row[6], row[7]
-                homexg = awayxg = None
+                    rg_season = None
             else:
-                gamedate, homename, score, awayname = row[2], row[4], row[5], row[6]
-                homexg = awayxg = None
+                if "Round" in table_formats:
+                    rg_season, gamedate, homename, score, awayname = row[0], row[3], row[5], row[6], row[7]
+                    homexg = awayxg = None
+                else:
+                    gamedate, homename, score, awayname = row[2], row[4], row[5], row[6]
+                    homexg = awayxg = rg_season = None
 
 
             try:
@@ -72,7 +71,7 @@ def parseSeason(teamlist: TeamList, filename, leaguename, leagueyear):
                 home_xg = away_xg = None
 
             # Make this a game
-            lg = EXTRA_LEAGUE if HAS_POSTSEASON and rg_season != "Regular Season" else league
+            lg = EXTRA_LEAGUE if rg_season and rg_season != "Regular Season" else league
             game = Game(date, lg, hometeam, awayteam)
             if not (homescore is None):
                 game.score(homescore, awayscore, home_xg, away_xg)
--- python/season_map.py
+++ python/season_map.py
@@ -4,10 +4,10 @@
 all_seasons = ["2014_2015", "2015_2016", "2016_2017", "2017_2018", "2018_2019", "2019_2020"]
 
 season_map = {
-"2014_2015": ["serie_a", "serie_b"],
-"2015_2016": ["serie_a", "serie_b"],
-"2016_2017": ["serie_a"],
-"2017_2018": ["serie_a", "serie_b"],
-"2018_2019": ["serie_a", "serie_b"],
-"2019_2020": ["serie_a", "serie_b"]
+"2014_2015": ["serie_a", "serie_b", "bdslga", "bdslga_2"],
+"2015_2016": ["serie_a", "serie_b", "bdslga", "bdslga_2"],
+"2016_2017": ["serie_a", "bdslga", "bdslga_2"],
+"2017_2018": ["serie_a", "serie_b", "bdslga", "bdslga_2"],
+"2018_2019": ["serie_a", "serie_b", "bdslga", "bdslga_2"],
+"2019_2020": ["serie_a", "serie_b", "bdslga", "bdslga_2"]
 }
